bf7476e2f8ac51f5bfbf06d477f6090d1cf1b147
aberkow <berkowitz.clarinet@gmail.com>
2016-06-29 10:06:57 -0400

    basic synth w no effects works

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -40132,16 +40132,26 @@
 
 	'use strict';
 	
-	var _tone = __webpack_require__(168);
-	
-	var _tone2 = _interopRequireDefault(_tone);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	var tone = __webpack_require__(168);
+	//synth needs to be created outside of nx.onload.
+	//otherwise the synth won't release the note.
+	var synth = new tone.MonoSynth().toMaster();
 	
 	nx.onload = function () {
 	  console.log('loaded');
 	  keyboard.on('*', function (data) {
 	    console.log(data);
+	    //create a new synth
+	    // var synth = new tone.MonoSynth().toMaster();
+	    var frequency;
+	    if (data.on > 0) {
+	      //convert the midi note to frequency
+	      frequency = nx.mtof(data.note);
+	      //trigger the synth at the frequency
+	      synth.triggerAttack(frequency);
+	    } else if (data.on === 0) {
+	      synth.triggerRelease();
+	    }
 	  });
 	};
 	
--- js/nxOnLoad.js
+++ js/nxOnLoad.js
@@ -1,9 +1,23 @@
-import tone from 'tone';
+var tone = require('tone');
+//synth needs to be created outside of nx.onload.
+//otherwise the synth won't release the note.
+var synth = new tone.MonoSynth().toMaster();
 
 nx.onload = function(){
   console.log('loaded');
   keyboard.on('*', function(data){
     console.log(data);
+    //create a new synth
+    // var synth = new tone.MonoSynth().toMaster();
+    var frequency;
+    if (data.on > 0){
+      //convert the midi note to frequency
+      frequency = nx.mtof(data.note);
+      //trigger the synth at the frequency
+      synth.triggerAttack(frequency);
+    } else if (data.on === 0) {
+      synth.triggerRelease();
+    }
   });
 }
 
