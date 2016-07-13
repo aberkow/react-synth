26a5e7148fb43898dd6401f8f07ddb27a92b3a4c
aberkow <berkowitz.clarinet@gmail.com>
2016-07-12 20:55:40 -0400

    components work but only when rendered a second time

--- components/Waveform.jsx
+++ components/Waveform.jsx
@@ -4,25 +4,25 @@ import tone from 'tone';
 
 //import nxWaveformOnLoad from '../js/waveform/nxWaveformOnLoad';
 
+//set sampler as a global variable. refactor to separate file later
+//a la duoSynth.js
+var sampler = new tone.Sampler({
+  0: '../js/waveform/studderSchool.wav'
+}, {
+  player: {
+    loop: true,
+  }
+}).toMaster();
+
 class Waveform extends React.Component{
   constructor(props){
     super(props);
   }
   componentWillMount(){
-    var sampler = new tone.Sampler({
-      0: '../js/waveform/studderSchool.wav'
-    }, {
-      player: {
-        loop: true,
-      }
-    }).toMaster();
-
-    //loading samples needs to occur outside nx.onload but during componentWillMount
     tone.Buffer.on('load', function(){
       waveform1.setBuffer(sampler._buffers[0]._buffer);
       waveform1.select(0, 500);
     });
-
     nx.onload = function(){
       //trigger the waveform sampler.
       toggle1.on('*', function(data){
@@ -37,6 +37,30 @@ class Waveform extends React.Component{
         sampler.player.setLoopPoints(data.starttime/1000, data.stoptime/1000);
       });
     }
+  }
+
+  componentDidMount(){
+
+    //loading samples needs to occur outside nx.onload but during componentWillMount
+    // tone.Buffer.on('load', function(){
+    //   waveform1.setBuffer(sampler._buffers[0]._buffer);
+    //   waveform1.select(0, 500);
+    // });
+
+    // nx.onload = function(){
+    //   //trigger the waveform sampler.
+    //   toggle1.on('*', function(data){
+    //     if (data.value === 1){
+    //       sampler.triggerAttack('0');
+    //     } else {
+    //       sampler.triggerRelease();
+    //     }
+    //   });
+    //   //control the length of the sampler loop.
+    //   waveform1.on('*', function(data){
+    //     sampler.player.setLoopPoints(data.starttime/1000, data.stoptime/1000);
+    //   });
+    // }
   }
   render(){
     return(
